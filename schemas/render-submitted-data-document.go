9806b2e8784d0a4aaccea459668699cb96af398b
cburns93 <cburns93@users.noreply.github.com>
2017-10-06 14:14:00 +0100

    Fix indentation inconsistency

--- schemas/render-submitted-data-document.go
+++ schemas/render-submitted-data-document.go
@@ -2,10 +2,10 @@ package schemas
 
 //  'render-submitted-data-document' topic struct
 type RenderSubmittedDataDocument struct {
-	ID           string `avro:"id"                                   json:"id"`
-	Resource     string `avro:"resource"                             json:"resource"`
-	ResourceID   string `avro:"resource_id"                          json:"resource_id"`
-	ContentType  string `avro:"content_type"                         json:"content_type"`
-	DocumentType string `avro:"document_type"                        json:"document_type"`
-	UserID       string `avro:"user_id"                              json:"user_id"`
+	ID                              string                          `avro:"id"                              json:"id"`
+	Resource                        string                          `avro:"resource"                        json:"resource"`
+	ResourceID                      string                          `avro:"resource_id"                     json:"resource_id"`
+	ContentType                     string                          `avro:"content_type"                    json:"content_type"`
+	DocumentType                    string                          `avro:"document_type"                   json:"document_type"`
+	UserID                          string                          `avro:"user_id"                         json:"user_id"`
 }
