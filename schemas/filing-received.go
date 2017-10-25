ac08e34e0769b621e547e58a3ca6bfa9732febec
cburns93 <cburns93@users.noreply.github.com>
2017-10-25 13:00:16 +0100

    correct spelling of Received

--- schemas/filing-received.go
+++ schemas/filing-received.go
@@ -5,13 +5,13 @@ type FilingReceived struct {
 	ApplicationID string                   `avro:"application_id"                  json:"application_id"`
 	Attempt       int32                    `avro:"attempt"                         json:"attempt"`
 	ChannelID     string                   `avro:"channel_id"                      json:"channel_id"`
-	Presenter     PresenterFilingRecieved  `avro:"presenter"                       json:"presenter"`
-	Submission    SubmissionFilingRecieved `avro:"published_at"                    json:"published_at"`
+	Presenter     PresenterFilingReceived  `avro:"presenter"                       json:"presenter"`
+	Submission    SubmissionFilingReceived `avro:"published_at"                    json:"published_at"`
 	Items         []Items                  `avro:"items"                           json:"items"`
 }
 
 // PresenterFilingReceived struct represents PresenterFilingReceived in the FilingReceived struct
-type PresenterFilingRecieved struct {
+type PresenterFilingReceived struct {
 	Forename string `avro:"forename"                        json:"forename"`
 	Language string `avro:"language"                        json:"language"`
 	Surname  string `avro:"surname"                         json:"surname"`
@@ -19,7 +19,7 @@ type PresenterFilingRecieved struct {
 }
 
 // SubmissionFilingReceived struct represents SubmissionFilingReceived in the FilingReceived struct
-type SubmissionFilingRecieved struct {
+type SubmissionFilingReceived struct {
 	CompanyNumber string `avro:"company_number"                  json:"company_number"`
 	CompanyName   string `avro:"company_name"                    json:"company_name"`
 	ReceivedAt    string `avro:"received_at"                     json:"received_at"`
